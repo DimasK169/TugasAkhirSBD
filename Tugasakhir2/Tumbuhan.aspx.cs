016fda9af0c4c75212b710fcf3d8e096df530a9c
Dimas Kenang Surya Ardani <dimaskenang@students.undip.ac.id>
2022-12-06 19:51:11 +0700

    Fix 1

--- Tugasakhir2/Tumbuhan.aspx.cs
+++ Tugasakhir2/Tumbuhan.aspx.cs
@@ -19,13 +19,28 @@ namespace Tugasakhir2.bootstrap
         DataTable T_dt = new DataTable();
         DataSet T_ds = new DataSet();
 
+        SqlConnection TKl_SqlConn = new SqlConnection(ClsModule.Conn.ToString());
+        SqlCommand TKl_cmd = new SqlCommand();
+        SqlDataAdapter TKl_sda = new SqlDataAdapter();
+        DataTable TKl_dt = new DataTable();
+        DataSet TKl_ds = new DataSet();
+
+        SqlConnection TKe_SqlConn = new SqlConnection(ClsModule.Conn.ToString());
+        SqlCommand TKe_cmd = new SqlCommand();
+        SqlDataAdapter TKe_sda = new SqlDataAdapter();
+        DataTable TKe_dt = new DataTable();
+        DataSet TKe_ds = new DataSet();
 
         protected void Page_Load(object sender, EventArgs e)
         {
             T_SqlConn.Open();
+            TKl_SqlConn.Open();
+            TKe_SqlConn.Open();
             if (!Page.IsPostBack)
             {
                 T_DataShow();
+                TKl_DataShow();
+                TKe_DataShow();
             }
         }
 
@@ -41,6 +56,33 @@ namespace Tugasakhir2.bootstrap
             T_GridView.DataBind();
             T_SqlConn.Close();
         }
+
+        public void TKl_DataShow()
+        {
+            TKl_ds = new DataSet();
+            TKl_cmd.CommandText = "Select * from Klasifikasi";
+            TKl_cmd.Connection = TKl_SqlConn;
+            TKl_sda = new SqlDataAdapter(TKl_cmd);
+            TKl_sda.Fill(TKl_ds);
+            TKl_cmd.ExecuteNonQuery();
+            TKl_GridView.DataSource = TKl_ds;
+            TKl_GridView.DataBind();
+            TKl_SqlConn.Close();
+        }
+
+        public void TKe_DataShow()
+        {
+            TKe_ds = new DataSet();
+            TKe_cmd.CommandText = "Select kegunaan_id, kegunaan from kegunaan where (IsDeleted = 0)";
+            TKe_cmd.Connection = TKe_SqlConn;
+            TKe_sda = new SqlDataAdapter(TKe_cmd);
+            TKe_sda.Fill(TKe_ds);
+            TKe_cmd.ExecuteNonQuery();
+            TKe_GridView.DataSource = TKe_ds;
+            TKe_GridView.DataBind();
+            TKe_SqlConn.Close();
+        }
+
 
 
         protected void btnAdd_Click(object sender, EventArgs e)
